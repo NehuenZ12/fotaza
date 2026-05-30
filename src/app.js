f24d451fc866ab9d7711db192eec6bc00777f04c
NehuenZ12 <aldonehuen123@gmail.com>
2026-05-30 20:04:47 -0300

    Conexion a DB

--- src/app.js
+++ src/app.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const path = require('path');
 
+const authRoutes = require('./routes/authRoutes');
+const sequelize = require('./config/database');
+
 const app = express();
 const PORT = 3000;
 
-// Configuración de PUG
+// Configuracion de PUG
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
-// Archivos estáticos
+// Archivos estaticos
 app.use(express.static('public'));
+app.use('/auth', authRoutes);
 
 app.get('/', (req, res) => {
     res.render('index');
 });
+
+sequelize.authenticate()
+    .then(() => {
+        console.log('Base de datos conectada');
+    })
+    .catch((error) => {
+        console.error('Error de conexión:', error);
+    });
 
 app.listen(PORT, () => {
     console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
