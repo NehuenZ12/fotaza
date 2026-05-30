d462baaae20577a33566130df397097e0d43cbb3
NehuenZ12 <aldonehuen123@gmail.com>
2026-05-30 17:21:13 -0300

    probando servidor

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Bienvenido a Fotaza');
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+});
