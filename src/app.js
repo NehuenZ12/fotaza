58b5912da8493836bc6b143294a25d4959c5ce18
NehuenZ12 <aldonehuen123@gmail.com>
2026-05-30 17:32:31 -0300

    Probando pug y servidor

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
+// Configuración de PUG
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// Archivos estáticos
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.send('Bienvenido a Fotaza');
+    res.render('index');
 });
 
 app.listen(PORT, () => {
