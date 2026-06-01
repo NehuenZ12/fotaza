d4545a6cafc5e3cdab8b8aa9b2199aa67e6b5821
NehuenZ12 <aldonehuen123@gmail.com>
2026-06-01 02:01:07 -0300

    Notificacion de comentario

--- src/controllers/valoracionController.js
+++ src/controllers/valoracionController.js
@@ -1,17 +1,22 @@
 const Valoracion = require('../models/Valoracion');
 const Publicacion = require('../models/Publicacion');
+const Usuario = require('../models/Usuario');
+const Notificacion = require('../models/Notificacion');
 
 const valorar = async (req, res) => {
 
     try {
 
-        const publicacion = await Publicacion.findByPk(
-            req.params.id
-        );
+        const publicacion =
+            await Publicacion.findByPk(
+                req.params.id
+            );
 
         if (!publicacion) {
 
-            return res.redirect('/publicaciones');
+            return res.redirect(
+                '/publicaciones'
+            );
 
         }
 
@@ -20,22 +25,30 @@ const valorar = async (req, res) => {
             req.session.usuarioId
         ) {
 
-            return res.redirect('/publicaciones');
+            return res.redirect(
+                '/publicaciones'
+            );
 
         }
 
-        const yaExiste = await Valoracion.findOne({
+        const yaExiste =
+            await Valoracion.findOne({
 
-            where: {
-                usuarioId: req.session.usuarioId,
-                publicacionId: req.params.id
-            }
+                where: {
+                    usuarioId:
+                        req.session.usuarioId,
 
-        });
+                    publicacionId:
+                        req.params.id
+                }
+
+            });
 
         if (yaExiste) {
 
-            return res.redirect('/publicaciones');
+            return res.redirect(
+                '/publicaciones'
+            );
 
         }
 
@@ -44,6 +57,22 @@ const valorar = async (req, res) => {
             puntaje: req.body.puntaje,
             usuarioId: req.session.usuarioId,
             publicacionId: req.params.id
+
+        });
+
+        const usuario =
+            await Usuario.findByPk(
+                req.session.usuarioId
+            );
+
+        await Notificacion.create({
+
+            mensaje:
+                usuario.nombre +
+                ' valoró tu publicación',
+
+            usuarioId:
+                publicacion.usuarioId
 
         });
 
